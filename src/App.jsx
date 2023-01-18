7ed95b4e49043285a617bcb1cc85615d06da1847
Tailon Ramos <tailonramos11@gmail.com>
2023-01-17 23:00:17 -0300

    Estrutura inicial finalizada

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+//PAGES
+import Results from './pages/Results'
+import Add from './pages/Add'
+
+//COMPONENTS
+import Container from './components/Container';
 
 function App() {
 
   return (
     <div className="App">
-      <h1>Dashboard</h1>
+      <BrowserRouter>
+        <Container>
+          <Routes>
+            <Route path='/results' element={<Results />} />
+            <Route path='/add' element={<Add />} />
+          </Routes>
+        </Container>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ src/pages/Add.jsx
@@ -0,0 +1,31 @@
+import styles from './Add.module.css'
+
+const Add = () => {
+	return (
+		<div className={styles.container_task}>
+			<h1>Adicionar Tarefa</h1>
+			<form >
+				<label htmlFor="member">
+					Membro da equipe:
+					<select name="member" id="member_select">
+						<option value="Ob_1345s">Sandra Joyce</option>
+						<option value="Ob_4s6d8">Martins Loyery</option>
+						<option value="Ob_s6d7e">Jonas Moreira</option>
+						<option value="Ob_w0e95">Maria Gutenberg</option>
+					</select>
+				</label>
+				<label htmlFor="task">
+					Origem da tarefa:
+					<select name="task" id="task_select">
+						<option value="1">gab1</option>
+						<option value="2">gab2</option>
+						<option value="3">gab3</option>
+					</select>
+				</label>
+				<input type="submit" value="Enviar" />
+			</form>
+		</div>
+	)
+}
+
+export default Add
