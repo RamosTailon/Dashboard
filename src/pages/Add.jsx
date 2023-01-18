a3830204cbc97a1c154a2a8a107bab1f8e1d1c87
Tailon Ramos <tailonramos11@gmail.com>
2023-01-17 23:55:05 -0300

    página de Add e Results

--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -4,10 +4,10 @@ const Add = () => {
 	return (
 		<div className={styles.container_task}>
 			<h1>Adicionar Tarefa</h1>
-			<form >
+			<form className={styles.form_task}>
 				<label htmlFor="member">
 					Membro da equipe:
-					<select name="member" id="member_select">
+					<select name="member" id="member_select" className={styles.select}>
 						<option value="Ob_1345s">Sandra Joyce</option>
 						<option value="Ob_4s6d8">Martins Loyery</option>
 						<option value="Ob_s6d7e">Jonas Moreira</option>
@@ -16,7 +16,7 @@ const Add = () => {
 				</label>
 				<label htmlFor="task">
 					Origem da tarefa:
-					<select name="task" id="task_select">
+					<select name="task" id="task_select" className={styles.select}>
 						<option value="1">gab1</option>
 						<option value="2">gab2</option>
 						<option value="3">gab3</option>
--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import styles from './Results.module.css'
+import { useState, useEffect } from 'react';
+
+const urlBaseProducts = 'http://localhost:3000/results'
 
 const Results = () => {
+
+	const [results, setResults] = useState([]);
+
+	async function fetchDataResults() {
+		await fetch(urlBaseProducts)
+			.then(resp => { return resp.json() })
+			.then(data => setResults(data))
+			.catch()
+	}
+
+	useEffect(() => {
+		fetchDataResults();
+	}, []);
+
 	return (
-		<div>
-			<h1>Results</h1>
+		<div className={styles.container_results}>
+			<h1>Resultados</h1>
+			{results && results.map((item) => (
+				<div className={styles.individual} key={item.id}>
+					<article>{item.name}</article>
+					<p>{item.concludes.length}</p>
+				</div>
+			))}
 		</div>
 	)
 }
