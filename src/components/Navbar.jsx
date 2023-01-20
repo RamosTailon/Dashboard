57bc4637cc39a77483732530cf7b4fa78a956c67
Tailon Ramos <tailonramos11@gmail.com>
2023-01-19 21:48:06 -0300

    Botão switch

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,10 @@
-import styles from './/Navbar.module.css'
+import styles from './Navbar.module.css'
 import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 const Navbar = () => {
+	const [checked, setChecked] = useState(false);
+
 	return (
 		<nav>
 			<div className={styles.logo}>
@@ -11,6 +14,10 @@ const Navbar = () => {
 			<ul>
 				<li><Link to='/results'>Resultados</Link></li>
 				<li><Link to='/add'>Adicionar</Link></li>
+				<li ><div id={styles.switch}>
+					<input type="checkbox" id={styles.input_slide}
+						onChange={(e) => setChecked(e.target.checked)} />
+				</div></li>
 			</ul>
 		</nav>
 	)
