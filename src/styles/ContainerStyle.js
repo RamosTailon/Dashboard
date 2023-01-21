8997ba3087fc49dacdb704d6171cfa92559a0de0
Tailon Ramos <tailonramos11@gmail.com>
2023-01-21 18:51:40 -0300

    Corrigido bug do css

--- src/styles/ContainerStyle.js
+++ src/styles/ContainerStyle.js
@@ -4,8 +4,8 @@ import styled from 'styled-components';
 const ContainerStyle = styled.div`
     min-height: 67vh;
     padding: 1em 2em 3em;
-    max-width: 1080px;
-    margin: 0 auto;
+    display: flex;
+    justify-content: center;
     background-color: ${props => props.theme.theme.body_primary};
     color:${props => props.theme.theme.font_color};
 `
